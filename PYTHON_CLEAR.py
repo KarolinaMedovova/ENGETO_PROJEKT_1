ab37263ce57a64d939b4495825f28466842261be
Karolina Medovova <student.karolina@seznam.cz>
2025-10-01 12:20:00 +0200

    Dokončení fce odstranit_ukol().

--- PYTHON_CLEAR.py
+++ PYTHON_CLEAR.py
@@ -1,31 +1,21 @@
 ukoly = []
 ocislovany_seznam_ukolu = []
-nazvy_ukolu = []
-popisy_ukolu = []
-nazev_a_popis_ukolu = []
 
 def hlavni_menu():
     print("Správce úkolů - Hlavní menu\n1. Přidat nový úkol\n2. Zobrazit všechny úkoly\n3. Odstranit úkol\n4. Konec programu")
     
 hlavni_menu()
 
-def ocislovat_ukoly():
-    for index, ukol in enumerate(ukoly, start=1):
-        print(f"{index}. {ukol}")
-    print(" ")
-    ocislovane_ukoly = (f"{index}. {ukol}")
-    ocislovany_seznam_ukolu.append(ocislovane_ukoly)
-
 def pridat_ukol():
     print(" ")
     nazev_ukolu = input("Zadejte název úkolu: ")
-    while nazev_ukolu.isspace() or nazev_ukolu == "":   #nebo nazev_ukolu == "", len(nazev_ukolu) == 0
+    while nazev_ukolu.isspace() or nazev_ukolu == "":
         print("Byl zadán prázdný vstup. Zadejte název úkolu.")
         print(" ")
         nazev_ukolu = input("Zadejte název úkolu: ")
         
     popis_ukolu = input("Zadejte popis úkolu: ")
-    while popis_ukolu.isspace() or popis_ukolu == "":   #nebo popis_ukolu == "", len(popis_ukolu) == 0
+    while popis_ukolu.isspace() or popis_ukolu == "":
         print("Byl zadán prázdný vstup. Zadejte popis úkolu.")
         print(" ")
         popis_ukolu = input("Zadejte popis úkolu: ")
@@ -34,15 +24,6 @@ def pridat_ukol():
     print(" ")
     hlavni_menu()
     ukoly.append(f"{nazev_ukolu} - {popis_ukolu}")
-    nazev_a_popis_ukolu.append(nazev_ukolu+" - "+popis_ukolu)
-"""   
-    nazvy_ukolu.append(nazev_ukolu)
-    popisy_ukolu.append(popis_ukolu)
-    
-    for index, ukol in enumerate(ukoly, start=1):
-        ocislovane_ukoly = (f"{index}. {ukol}")
-        ocislovany_seznam_ukolu.append(ocislovane_ukoly)
-"""
 
 def zobrazit_ukoly():
     print(" ")
@@ -60,19 +41,18 @@ def odstranit_ukol():
         print(ocislovane_ukoly)
     ocislovany_seznam_ukolu.append(ocislovane_ukoly)
     print(" ")
-    task_number = int(input("Zadejte číslo úkolu, který chcete odstranit: "))
 
-    while type(task_number) is not int or task_number <1: #or task_number == "" or task_number.isspace():
+    task_number = input("Zadejte číslo úkolu, který chcete odstranit: ")
+    while not task_number.isdigit() or int(task_number) < 1 or int(task_number) > len(ukoly):
         print("Bylo zadáno neexistující číslo úkolu.")
-        task_number = int(input("Zadejte číslo úkolu, který chcete odstranit: "))
+        task_number = input("Zadejte číslo úkolu, který chcete odstranit: ")
 
     task_index = int(task_number) -1
     odstraneny = ukoly.pop(task_index)
     nazev = odstraneny.split(" - ")[0]
-
     print(f"Úkol '{nazev}' byl odstraněn.")
     print(" ")
-    print("Správce úkolů - Hlavní menu\n1. Přidat nový úkol\n2. Zobrazit všechny úkoly\n3. Odstranit úkol\n4. Konec programu")
+    hlavni_menu()
 
 def konec_programu():
     print(" ")
