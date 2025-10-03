17d6c1ade577c0bdb241d70e4bb6c41f0eecc7bb
Karolina Medovova <student.karolina@seznam.cz>
2025-10-03 09:20:10 +0200

    Přidána podmínka při neúmyslném odstranění úkolu.

--- PYTHON_CLEAR.py
+++ PYTHON_CLEAR.py
@@ -1,10 +1,9 @@
 ukoly = []
-ocislovany_seznam_ukolu = []
 
 def hlavni_menu():
+    print(" ")
     print("Správce úkolů - Hlavní menu\n1. Přidat nový úkol\n2. Zobrazit všechny úkoly\n3. Odstranit úkol\n4. Konec programu")
-    
-hlavni_menu()
+
 
 def pridat_ukol():
     print(" ")
@@ -21,49 +20,44 @@ def pridat_ukol():
         popis_ukolu = input("Zadejte popis úkolu: ")
 
     print(f"Úkol '{nazev_ukolu}' byl přidán.")
-    print(" ")
-    hlavni_menu()
     ukoly.append(f"{nazev_ukolu} - {popis_ukolu}")
+
 
 def zobrazit_ukoly():
     print(" ")
     if not ukoly:
         print("Seznam úkolů je prázdný.")
-        print(" ")
         return
     
     print("Seznam úkolů:")
     for index, ukol in enumerate(ukoly, start=1):
         print(f"{index}. {ukol}")
-    print(" ")
-    hlavni_menu()
 
-def odstranit_ukol():
+
+def odstranit_ukol():#
     if not ukoly:
         print(" ")
-        print("Seznam úkolů je prázdný. Prosím, zadejte možnost 1 nebo 4.")
-        print(" ")
-        hlavni_menu()
+        print("Seznam úkolů je prázdný, žádný úkol nelze odstranit. Prosím, zadejte možnost 1 nebo 4.")
         return
     print(" ")
     print("Seznam úkolů:")
     for index, work in enumerate(ukoly, start=1): 
         ocislovane_ukoly = (f"{index}. {work}")
         print(ocislovane_ukoly)
-    ocislovany_seznam_ukolu.append(ocislovane_ukoly)
     print(" ")
 
-    task_number = input("Zadejte číslo úkolu, který chcete odstranit: ")
-    while not task_number.isdigit() or int(task_number) < 1 or int(task_number) > len(ukoly):
+    task_number = input("Zadejte číslo úkolu, který chcete odstranit. (Pro návrat do hlavního menu zadejte 'x'.): ")
+    if task_number == "x":
+        return
+    elif not task_number.isdigit() or int(task_number) < 1 or int(task_number) > len(ukoly):
         print("Bylo zadáno neexistující číslo úkolu.")
         task_number = input("Zadejte číslo úkolu, který chcete odstranit: ")
-
+    
     task_index = int(task_number) -1
     odstraneny = ukoly.pop(task_index)
     nazev = odstraneny.split(" - ")[0]
     print(f"Úkol '{nazev}' byl odstraněn.")
-    print(" ")
-    hlavni_menu()
+
 
 def konec_programu():
     print(" ")
@@ -71,6 +65,7 @@ def konec_programu():
 
 
 while True:
+    hlavni_menu()
     option = input("Vyberte možnost (1-4): ")
     if option == "1":
         pridat_ukol()  
@@ -82,4 +77,5 @@ while True:
         konec_programu()
         break
     else:
+        print(" ")
         print("Byla zadána neplatná volba. Prosím, zvolte možnost 1, 2, 3 nebo 4.")
